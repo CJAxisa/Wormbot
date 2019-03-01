a51a82b79df55d731ded41746fa6db616b32acd5
John Shull <JohnShull@mail.rit.edu>
2019-02-28 19:52:21 -0500

    Initial Scene and Movement Setup
    Created a test scene, floor, and started movement and camera scripts

---
+++ Wormbot/Assets/MovementScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody;
+    private float wormyMotionTimer;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetKey("d") && !Input.GetKey("a")) 
+        {
+            rigidbody.AddForce(new Vector2((Mathf.Sin(wormyMotionTimer++) + 1f) * 5f, 0));
+        }
+        else if (Input.GetKey("a"))
+        {
+            rigidbody.AddForce(new Vector2((Mathf.Sin(wormyMotionTimer++) + 1f) * -5f, 0));
+        }
+
+    }
+}
