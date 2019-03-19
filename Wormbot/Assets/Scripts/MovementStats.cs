cb507e576ba08d8b644318c6d2a72ffddfe18aeb
Christian Axisa (RIT Student) <cxa7619@ad.rit.edu>
2019-03-19 11:56:49 -0400

    Changed Jump controls to be analogue

---
+++ Wormbot/Assets/Scripts/MovementStats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementStats : MonoBehaviour
+{
+
+    public float jumpVelMult;
+    public float maxMoveSpeed;
+    public int numJumps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
