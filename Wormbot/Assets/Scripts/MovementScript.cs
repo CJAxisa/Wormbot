3070dfa1ecdddd3c985dc4bf0b1687282fc48314
RickMcLightning <oceanic.plantains@gmail.com>
2019-04-02 00:50:43 -0400

    Revert "syncing with master"
    This reverts commit 64d8266bbb2e8f8ed820dd59a2654936b4e5a8e8, reversing
    changes made to d1eb36e2e15e03dede1c098498df4aca516405ef.

--- Wormbot/Assets/Scripts/MovementScript.cs
+++ Wormbot/Assets/Scripts/MovementScript.cs
@@ -36,9 +36,8 @@ public class MovementScript : MonoBehaviour
     private Vector2 startingColliderSize;
     private Vector2 startingColliderOffset;
 
-    private Vector3 leftScale;
-    private Vector3 rightScale;
-    
+
+
 
     void Start()
     {
@@ -48,20 +47,24 @@ public class MovementScript : MonoBehaviour
         jumpsLeft = numJumps;
         captured = false;
 
-
         distanceToGround = GetComponent<BoxCollider2D>().bounds.extents.y;
         lm = LayerMask.GetMask("Ground");
 
+        animator.SetTrigger("prepairingToJump");
+        animator.SetTrigger("jumping");
+        animator.SetTrigger("landed");
         startingSprite = gameObject.GetComponent<SpriteRenderer>().sprite;
         startingScale = transform.lossyScale;
+        
+        //
 
         setMovementStats(gameObject.GetComponent<MovementStats>());
         startingColliderSize = gameObject.GetComponent<BoxCollider2D>().size;
-        startingColliderOffset = gameObject.GetComponent<BoxCollider2D>().offset;
+        startingColliderOffset = gameObject.GetComponent<BoxCollider2D>().offset; 
 
-        leftScale = transform.localScale;
-        rightScale = transform.localScale;
-        rightScale.x *= -1;
+
+
+
     }
 
     void Update()
@@ -69,7 +72,6 @@ public class MovementScript : MonoBehaviour
         if (Input.GetKey("d") && !Input.GetKey("a"))
         {
             walkVelocity = new Vector2(1f * moveSpeedMultiplier, 0f);
-            transform.localScale = rightScale;
             if (facingLeft)
                 facingLeft = false;
         }
@@ -77,7 +79,6 @@ public class MovementScript : MonoBehaviour
         {
             //rigidbody.AddForce(new Vector2((Mathf.Sin(wormyMotionTimer++) + 1f) * -5f, 0));
             walkVelocity = new Vector2(-1f * moveSpeedMultiplier, 0f);
-            transform.localScale = leftScale;
             if (!facingLeft)
                 facingLeft = true;
         }
@@ -113,12 +114,11 @@ public class MovementScript : MonoBehaviour
         if (Input.GetMouseButton(0) && jumpsLeft != 0)
         {
             Debug.DrawLine(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), Color.cyan);
-            animator.SetBool("jumpPrep", true);
+            animator.SetTrigger("prepJump");
         }
 
         if (Input.GetMouseButtonUp(0) && jumpsLeft != 0)
         {
-            animator.SetBool("jumpPrep", false);
             jumpVel = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
             jumpVel.z = 0f;
             if (jumpVel.magnitude > 6f)
@@ -155,6 +155,8 @@ public class MovementScript : MonoBehaviour
 
             jumpsLeft--;
             grounded = false;
+
+            animator.SetTrigger("jump");
         }
     }
 
