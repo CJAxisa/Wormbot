71b726246a1e75b1545d94dac18f7710177153b7
Christian Axisa (RIT Student) <cxa7619@ad.rit.edu>
2019-10-29 13:50:02 -0400

    starting timer

---
+++ Wormbot/Assets/Scripts/Timer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+
+
+    public float timeSoFar;
+    public string displayTime;
+    public static bool isCountingTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeSoFar = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isCountingTime)
+            timeSoFar += Time.deltaTime;
+
+
+        displayTime = (timeSoFar / 60) + ":" + Mathf.Floor(timeSoFar%60)+"."+Mathf.Round((timeSoFar-Mathf.Floor(timeSoFar))*100)/100;
+
+    }
+
+
+    public void resetTimer()
+    {
+        timeSoFar = 0f;
+    }
+}
